8546130284a5090da586ee8aab002efc4064704d
Paul Matlashewski <pmatlashewski@gmail.com>
2021-06-15 09:04:42 -0700

    Update layout

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -4,19 +4,17 @@ import {
   navbar,
   navbarText,
   navbarItem,
-  navbarLinks } from './navbar.module.css'
+  navbarLinks
+} from './navbar.module.css'
 
 const Navbar = () => {
   return (
     <div className={navbar}>
       <div className={navbarItem}>
-        <span className={navbarText}>Paul Matlashewski</span>
+        <Link to="/" className={navbarText}>Paul Matlashewski</Link>
       </div>
       <nav>
         <ul className={navbarLinks}>
-          <li className={navbarItem}>
-            <Link to="/" className={navbarText}>Home Page</Link>
-          </li>
           <li className={navbarItem}>
             <Link to="/about" className={navbarText}>About</Link>
           </li>
